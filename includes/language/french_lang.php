2e5f14e29a16d504cd4d7f437dac8d1c437f03ff
Ayodele Adedayo <ayodelea@proxynetgroup.local>
2021-04-12 14:36:01 +0100

    updated translation

---
+++ includes/language/french_lang.php
@@ -0,0 +1,160 @@
+<?php
+$lang['Welcome to Proxynet Communications'] = "Bienvenue chez Proxynet Communications";
+$lang['welcome_message2'] = 'Obtenez la meilleure offre sur les prix de vos produits Logitech';
+$lang['shop_now'] = 'Achetez maintenant';
+$lang['Logitech Rally for'] = 'Logitech Rally pour les grandes salles de conférence';
+$lang['logitech_group'] = 'Groupe Logitech';
+$lang['enterprice_solution'] = 'Solutions d"entreprise';
+$lang['Hot Products'] = 'Produits chauds';
+$lang['exclusive_products'] = 'Produits exclusifs';
+
+$lang['New Arrival'] = 'Nouvelle arrivee';
+
+$lang['best_sellers'] = 'Meilleurs vendeurs';
+$lang['special_offers'] = 'Offre spéciale';
+
+$lang['Latest News'] = 'Dernières actualités';
+$lang['Read up on the latest news'] = 'Renseignez-vous sur les dernières actualités de Promallshop';
+
+$lang['Join Our Newsletter Now!!'] = 'Rejoignez notre newsletter maintenant !!';
+$lang['And get updates on New Products'] = 'Et obtenez des mises à jour sur les nouveaux produits.';
+
+$lang['Logitech Group Solution'] = 'Solution du groupe Logitech';
+$lang['Get the Resellers Advantage'] = 'Obtenez l"avantage des revendeurs';
+$lang['Fast Tracking'] = 'Suivi rapide pour la livraison';
+$lang['We Ensure All'] = 'Nous nous assurons que tous nos produits seront suivis correctement afin de nous assurer qu"ils vous parviennent en toute sécurité.';
+$lang['Guarantee Of'] = 'Garantie des produits';
+$lang['We ensure replacements'] = 'Nous garantissons la garantie standard et le remplacement de tous les produits Logitech défectueux à nos revendeurs.';
+$lang['27/4 Support'] = 'Assistance 27/4';
+$lang['To all our Resellers'] = 'À tous nos revendeurs, nous offrons une assistance clientèle 24/7.';
+$lang['Enter Email'] = 'Entrer l"adresse e-mail.';
+$lang['A Pan-African Dristribution'] = 'Une société panafricaine de distribution, un distributeur à valeur ajoutée de grandes marques mondiales.';
+$lang['Useful Links'] = 'Liens utiles';
+$lang['About Us'] = 'À propos de nous';
+$lang['Contact Us'] = 'Nous contacter';
+$lang['Terms & Conditions'] = 'termes et conditions';
+$lang['Privacy Policy'] = 'Politique de confidentialité';
+$lang['Shop'] = 'Magasin';
+$lang['My Account'] = 'Mon compte';
+$lang['Login'] = 'S"identifier';
+$lang['Register'] = 'S"inscrire';
+$lang['Contact Info'] = 'Informations de contact';
+$lang['ALL RIGHTS RESERVED.'] = 'TOUS DROITS RÉSERVÉS.';
+$lang['phone'] = 'téléphone';
+$lang['Get In Touch'] = 'Entrer en contact';
+$lang['We would Love to hear'] = 'Nous aimerions avoir de vos nouvelles, demandes de renseignements, observations, passer une commande.';
+$lang['Enter Name'] = 'Entrez le nom';
+$lang['Enter Phone No.'] = 'Entrez le numéro de téléphone';
+$lang['Enter Subject '] = 'Entrez le sujet';
+$lang['Enter Email'] = 'Entrez votre e-mail';
+$lang['Message'] = 'Message';
+$lang['Send Message'] = 'Envoyer le message';
+$lang['search'] = 'chercher';
+$lang['Register As a Reseller!'] = 'Inscrivez-vous en tant que revendeur!';
+$lang['categories'] = 'Catégories';
+$lang['Home'] = 'Accueil';
+$lang['Cart is empty. Add  product to cart to continue'] = 'Le panier est vide. Ajouter le produit au panier pour continuer';
+$lang['Profile information has been updated!'] = 'Les informations de profil ont été mises à jour!';
+$lang['Verification document succesfully submitted'] = 'Document de vérification soumis avec succès';
+$lang['No file has been selected'] = "Aucun fichier n'a été sélectionné";
+$lang['Invalid account'] = 'Compte invalide';
+$lang['Old password is invalid'] = "L'ancien mot de passe n'est pas valide";
+$lang['Password does not match'] = "Le mot de passe ne correspond pas";
+$lang['Invalid request'] = "Requête invalide";
+$lang['Search result for:'] = 'Résultat de la recherche pour:';
+$lang['Logitech Products Store'] = 'Magasin de produits Logitech';
+$lang['Subscribe to Our Newsletter'] = 'Abonnez-vous à notre newsletter';
+$lang['Subscribe'] = 'Souscrire';
+$lang['No product found'] = 'Aucun produit trouvé';
+$lang['Search'] = 'Chercher';
+$lang['Email is invalid'] = 'Le courriel est invalide';
+$lang['Register'] = "S'inscrire";
+$lang['Account creation successful'] = 'Création de compte réussie';
+$lang['First stage of registration completed! kindly login to your account to complete the registration process'] = "Première étape d'inscription terminée! veuillez vous connecter à votre compte pour terminer le processus d'inscription";
+$lang['Create an Account'] = 'Créer un compte';
+$lang['Enter Your Name'] = 'Entrez le nom de votre entreprise';
+$lang['Enter Your Company Name'] = 'Entrez le nom de votre entreprise';
+$lang['Enter Your Email'] = 'Entrer votre Email';
+$lang['Enter Your Phone Number'] = 'Entrez votre numéro de téléphone';
+$lang['Password'] = 'Mot de passe';
+$lang['Confirm Password'] = 'Confirmez le mot de passe';
+$lang['Already have an account?'] = 'Vous avez déjà un compte?';
+$lang['Email is already in use'] = 'cet email est déjà utilisé';
+$lang['Email is invalid'] = 'Le courriel est invalide';
+
+$lang['Login To your Account!'] = 'Connectez-vous à votre compte!';
+$lang['Remember me'] = 'Souviens-toi de moi';
+$lang['Forgot password?'] = 'Mot de passe oublié?';
+$lang["Don't Have an Account?"] = "Vous n'avez pas de compte?";
+$lang["Sign up now"] = "S'inscrire maintenant";
+$lang["Enter Password"] = "Entrer le mot de passe";
+
+$lang["Invalid login details"] = "Informations de connexion invalide";
+$lang["Login successful!"] = "Connexion réussie!";
+$lang["myaccount"] = "My Account";
+$lang["Welcome"] = "Bienvenu";
+$lang["Account Status:"] = "Statut du compte";
+$lang["Your account has"] = "Votre compte n'a pas été vérifié, veuillez télécharger le formulaire de vérification ci-dessous, remplir hors ligne et soumettre";
+$lang["Your verified form"] = "Votre formulaire de vérification est actuellement en cours d'examen.";
+$lang["Your verification document"] = "Votre document de vérification a été rejeté, car il ne répondait pas aux exigences. Veuillez télécharger un nouveau format et le remplir correctement, après quoi vous pourrez le télécharger à nouveau.";
+$lang["Only pdf and doc/docx"] = "Seuls les documents pdf et doc / docx sont pris en charge";
+$lang["Submit"] = "Soumettre";
+$lang["Orders"] = "Ordres";
+$lang["Order"] = "Ordre";
+$lang["Date"] = "Date";
+$lang["Status"] = "Statut";
+$lang["Total"] = "Total";
+$lang["Discount"] = "Remise";
+$lang["Actions"] = "Actions";
+$lang["Account Details"] = "Détails du compte";
+$lang["Company Name"] = "Nom de l'entreprise";
+$lang["Name"] = "Nom";
+$lang["Email Address"] = "Adresse e-mail";
+$lang["Phone Number "] = "Numéro de téléphone";
+$lang["Billing Address"] = "adresse de facturation";
+$lang["Ghana"] = "Ghana";
+$lang["Ivory Coast"] = "Côte d'Ivoire";
+$lang["Nigeria"] = "Nigeria";
+$lang["Save"] = "sauver";
+$lang["Change Password"] = "Changer le mot de passe";
+$lang["Current Password"] = "Mot de passe actuel";
+$lang["New Password"] = "nouveau mot de passe";
+$lang["Confirm Password"] = "Confirmez le mot de passe";
+$lang["Category"] = "Catégorie";
+
+$lang["Access to Logitech University"] = "Accès à l'Université Logitech";
+$lang["Logitech University Content"] = "Les revendeurs enregistrés auront accès à l'université Logitech pour acquérir plus de connaissances et de ressources sur les solutions de visioconférence Logitech";
+$lang["Resellers Category and incentives"] = "Catégorie de revendeurs et incitations";
+$lang["We offer product discount"] = "Nous offrons des rabais sur les produits en fonction de la catégorie des revendeurs";
+$lang["Total Order"] = "Commande totale";
+$lang["Referral Link"] = "Lien de référence";
+$lang["Referrals"] = "Références";
+$lang["Resellers Referred"] = "Revendeurs recommandés";
+$lang["Referral Earnings"] = "Referral Earnings";
+$lang["Total Referrals"] = "Total Referral";
+$lang["Active Referrals"] = "Active Referral";
+
+$lang["Please write your review"] = "Veuillez écrire votre avis";
+$lang["Rating has been submitted!"] = "La note a été soumise!";
+$lang["You have already subscribed"] = "Vous êtes déjà abonné";
+
+$lang["Subscription succsessful"] = "Abonnement réussi";
+$lang["We offer product discount"] = "Nous offrons des remises et des incitations à tous nos revendeurs en fonction de différentes catégories de revendeurs";
+
+
+$lang["Blog"] = "Blog";
+$lang["Comment"] = "Commenter";
+$lang["Comments"] = "commentaires";
+$lang["Write a comment"] = "Écrire un commentaire";
+$lang["Recent Posts"] = "Messages récents";
+$lang["Use Anonymous"] = "Utiliser anonyme";
+$lang["submit"] = "soumettre";
+$lang["Affordable Prices"] = "Prix abordables";
+
+$lang['5B Adedeji Close, Opebi Ikeja, Lagos.'] = "1er Étage, Immeuble SIAA Cocody 2 Plateaux, 7e Tranche, carrefour Vase d’Honneur en face de l’hôtel la FORGE";
+$lang['+234 803 473 8104'] = "+225 27 22 46 84 34";
+
+$lang["Promallshop is"] = "Promallshop est une plate-forme de commerce électronique traitant des solutions Logitech telles que la vidéoconférence, les claviers, la souris, les casques et bien d'autres pour votre usage quotidien.";
+
+
+
